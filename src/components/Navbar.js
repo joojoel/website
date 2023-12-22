2dbc6a30f41f286a817ffd839f19416e4196e9c1
Joel <joel@latitudeE6530>
2023-12-22 08:19:58 +0200

    small changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -37,7 +37,7 @@ function Navbar() {
       <nav className='navbar'>
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-            JOEL <i class="fa-solid fa-circle-check"></i>
+            <i class="fa-solid fa-arrow-up"></i>
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fa-solid fa-times' : 'fa-solid fa-bars'} />
